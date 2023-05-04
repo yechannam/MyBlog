bafd82b0d7a188f6d1bc701a5379e37b20814db6
Nam Yechan <chanmias0318@gmail.com>
2023-05-04 21:31:40 +0900

    feat : blog func

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { FunctionComponent } from "react";
+
+const FooterWrapper = styled.div`
+	display: grid;
+	place-items: center;
+	margin-top: auto;
+	padding: 50px 0;
+	font-size: 15px;
+	text-align: center;
+	line-height: 1.5;
+
+	@media (max-width: 768px) {
+		font-size: 13px;
+	}
+`
+
+const Footer: FunctionComponent = () => {
+	return (
+		<FooterWrapper>
+			Thank you for visiting my blog,
+			Have a good day :)
+			<br /> 2023 Developer yecn, Powerd by Gatsby
+		</FooterWrapper>
+	)
+}
+
+export default Footer
---
+++ src/pages/index.tsx
@@ -0,0 +1,121 @@
+import React, { FunctionComponent, useMemo } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import CategoryList, { CategoryListProps } from 'components/Main/CategoryList'
+import Introduction from 'components/Main/Introduction'
+import PostList from 'components/Main/PostList'
+import { graphql } from 'gatsby'
+import { PostListItemType as PostType } from 'types/PostItem.types'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import queryString, { ParsedQuery } from 'query-string'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+
+type IndexPageProps = {
+  location: {
+    search: string
+  }
+  data: {
+    allMarkdownRemark: {
+      edges: PostType[]
+    }
+    file: {
+      childImageSharp: {
+        gatsbyImageData: IGatsbyImageData
+      }
+    }
+  }
+}
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  location: {search},
+	data: {
+		allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+    },
+	},
+}) {
+  const parsed: ParsedQuery<string> = queryString.parse(search)
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'],
+          {
+            node: {
+              frontmatter: {categories},
+            },
+          }: PostType,
+        ) => {
+          categories.forEach(category => {
+            if (list[category] === undefined) list[category] = 1;
+            else list[category]++;
+          });
+
+          list['All']++;
+
+          return list;
+        },
+        {All:0},
+      ),
+    [],
+  )
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction profileImage={gatsbyImageData} />
+      <CategoryList selectedCategory={selectedCategory} categoryList={CATEGORY_LIST} />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
+      <Footer />
+    </Container>
+  )
+}
+
+export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+          }
+        }
+      }
+    }
+    file(name: { eq: "download" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
+      }
+    }
+  }
+`
